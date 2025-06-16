0298a777ff42c1916e22b3b766b79a7625102a86
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-06-17 01:24:59 +0530

    conditional statement written in jsx file

--- src/Components/Hello.jsx
+++ src/Components/Hello.jsx
@@ -7,18 +7,38 @@ import React from "react";
 //     </div>)
 // }
 
-let name = "Bob";
-const car_obj = {name:"Rolls Royce", color:"Blue"};
+// let name = "Bob";
+// const car_obj = {name:"Rolls Royce", color:"Blue"};
+// function Hello(){
+//     return(<div>
+//         <h1>Hello JSX</h1>
+//         {/* simple way to write expression or variable or poperty or obj or any valied js expression  using {<variable or exression>} */}
+//         <h1>{name}</h1> 
+//         <h1>{5+5}</h1> 
+//         <h1>5+5</h1>
+//         {/* {{ }} is common syntax in JSX (used with React) or similar templating engines. The outer curly braces indicate that you're embedding a JavaScript expression, and the inner curly braces define a JavaScript object. */}
+//         <h1 style={{color:car_obj.color}}>{car_obj.name}</h1>
+//         {/* this is a fragment (one top elements) */}
+//         <></> 
+//     </div>)
+// }
+
+let a= 10;
+let msg="";
+// the conditional statement has to be put outside of the jsx function
+if(a>50){
+    msg="Hello";
+}else{
+    msg="Bye";
+}
+
+// JSX
 function Hello(){
     return(<div>
         <h1>Hello JSX</h1>
-        {/* simple way to write expression or variable or poperty or obj or any valied js expression  using {<variable or exression>} */}
-        <h1>{name}</h1> 
-        <h1>{5+5}</h1> 
-        <h1>5+5</h1>
-        {/* {{ }} is common syntax in JSX (used with React) or similar templating engines. The outer curly braces indicate that you're embedding a JavaScript expression, and the inner curly braces define a JavaScript object. */}
-        <h1 style={{color:car_obj.color}}>{car_obj.name}</h1>
+        <h1>{msg}</h1>
     </div>)
 }
+
 
 export default Hello
