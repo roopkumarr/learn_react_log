2e6120e623addb6f859e0462565a831f3eac48e4
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-06-16 21:35:12 +0530

    made a simple functional component

---
+++ src/Components/Greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+// A simple functional component with all ways of writing a function
+// function Greet(){
+//     return <h1>Hello World</h1>
+// }
+// const Greet = function (){
+//     return <h1>Hello World!</h1>
+// }
+const Greet = () => {
+    return <h1>Hello World!!</h1>
+}
+export default Greet
