595c14d131738c7d17ccea2ff4e7eb24527df023
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-06-18 02:08:51 +0530

    tried Destructring props in a functional component

--- src/Components/Customer.js
+++ src/Components/Customer.js
@@ -1,23 +1,51 @@
 import React, { Component } from "react";
-class Customer extends Component{
-    constructor(){
-        super();
-        this.state={
-            sname:"RK",
-            sage:70
-        }
-    }
-    render(){
-        const {name, age} = this.props;
-        const {sname, sage} = this.state;
-        return(
-            <div>
-                <h1>{name}</h1>
-                <h1>{age}</h1>
-                <h1>{sname}</h1>
-                <h1>{sage}</h1>
-            </div>
-        )
-    }
+// class Customer extends Component{
+//     constructor(){
+//         super();
+//         this.state={
+//             sname:"RK",
+//             sage:70
+//         }
+//     }
+//     render(){
+//         const {name, age} = this.props;
+//         const {sname, sage} = this.state;
+//         return(
+//             <div>
+//                 <h1>{name}</h1>
+//                 <h1>{age}</h1>
+//                 <h1>{sname}</h1>
+//                 <h1>{sage}</h1>
+//             </div>
+//         )
+//     }
+// }
+// ===============Functional component
+// function Customer(props){
+//     return(
+//         <div>
+//             <h1>{props.name}</h1>
+//             <h1>{props.age}</h1>
+//         </div>
+//     )
+// }
+
+// function Customer(props){
+//     const {name, age} = props;
+//     return(
+//         <div>
+//             <h1>{name}</h1>
+//             <h1>{age}</h1>
+//         </div>
+//     )
+// }
+
+function Customer({name, age}){
+    return(
+        <div>
+            <h1>{name}</h1>
+            <h1>{age}</h1>
+        </div>
+    )
 }
 export default Customer;
