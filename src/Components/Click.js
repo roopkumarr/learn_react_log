195d2fe2ef0186258c69c7a2e2e73a718a3ca8b6
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-06-26 00:15:03 +0530

    simple component to show click event

--- src/Components/Click.js
+++ src/Components/Click.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Click(){
-    function HelloFunction(){
-        return(alert("Hello World"))
+// function Click(){
+//     function HelloFunction(){
+//         return(alert("Hello World"))
+//     }
+//     return(
+//         <div>
+//             <button onClick={HelloFunction}>Click</button>
+//         </div>
+//     )
+// }
+
+class Click extends Component{
+    HelloFunction(){
+        return(alert("Hello World"))        
     }
-    return(
+    render(){
+        return(
         <div>
-            <button onClick={HelloFunction}>Click</button>
+            <button onClick={this.HelloFunction}>Click</button>
         </div>
-    )
+        )
+    }
 }
 
 export default Click;
