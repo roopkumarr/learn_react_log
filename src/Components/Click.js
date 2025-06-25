9f0a16369f45d6a5f4c0b97d2328568cd72600a8
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-06-26 00:10:44 +0530

    formatted the code

--- src/Components/Click.js
+++ src/Components/Click.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-function Click (){
-    
-function HelloFunction(){
-    return(alert("Hello World"))
-}
+function Click(){
+    function HelloFunction(){
+        return(alert("Hello World"))
+    }
     return(
         <div>
             <button onClick={HelloFunction}>Click</button>
