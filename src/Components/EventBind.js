fa5aefc4cd3917fcb60e437f058c1a178a9ddbfb
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-06-27 01:06:26 +0530

    Implementing bind using arrow function

--- src/Components/EventBind.js
+++ src/Components/EventBind.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 // the main differece between calling bind inside constuctor and render is 
 // when it is called inside the button event in our case went ever the button is clicked the bind method is called again and again (Render method)
 // instead we can call it in constructor so it bind only once.  (In constructor)
+// Using Arrow fuction would be preferably the best way to bind because we don't need to bind the 'this' Object  (Arrow function) 
 class EventBind extends Component{
     constructor(){
         super();
@@ -12,7 +13,25 @@ class EventBind extends Component{
             name: "World"
         }
     }
-    HelloEvent(){
+    // HelloEvent(){
+    //     // console.log("this ====>", this);
+    //     this.setState({
+    //         name: "Globe"
+    //     })
+    // }
+    // render(){
+    //     return(
+    //         <div>
+    //             <h1>{this.state.name}</h1>
+    //             {/* Render method */}
+    //             {/* other alternative but always prefered to use the bind method inside constructor */}
+    //             <button onClick={this.HelloEvent.bind(this)}>Click11</button> 
+    //         </div>
+    //     )
+    // }
+    
+    // Using the Arrow function
+    HelloEvent = ()=>{
         // console.log("this ====>", this);
         this.setState({
             name: "Globe"
@@ -22,9 +41,7 @@ class EventBind extends Component{
         return(
             <div>
                 <h1>{this.state.name}</h1>
-                {/* Render method */}
-                {/* other alternative but always prefered to use the bind method inside constructor */}
-                <button onClick={this.HelloEvent.bind(this)}>Click11</button> 
+                <button onClick={this.HelloEvent}>Click1</button> 
             </div>
         )
     }
