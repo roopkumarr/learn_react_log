059f1b50881ac18e0eaa59b1bcdc16ced2196a0b
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-03 01:19:09 +0530

    better way of writting multiple properties for a html component and us it

--- src/Components/StyleComponent.js
+++ src/Components/StyleComponent.js
@@ -2,10 +2,16 @@ import React,{Component} from "react";
 
 class StyleComponent extends Component {
     render(){
+        // if there are multiple properties to be added we can define a simple obj and use in the style
+        let obj ={
+            color:"red",
+            backgroundColor:"yellow"
+        };
         return(
             <div>
                 {/* first set of curly brace is for JS expression and secand one is for JS object  */}
-                <h1 style={{color:"red", backgroundColor:"yellow"}}>Hello EveryOne</h1>
+                {/* <h1 style={{color:"red", backgroundColor:"yellow"}}>Hello EveryOne</h1> */}
+                <h1 style={obj}>Hello EveryOne</h1>
             </div>
         );
     }
