a8255383569780854f5c4d72159216e3e596c67f
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-03 02:00:01 +0530

    implementing the updation of style properties based on condition

--- src/Components/StyleComponent.js
+++ src/Components/StyleComponent.js
@@ -1,22 +1,42 @@
 import React,{Component} from "react";
 
 class StyleComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            isActive:false
+        }
+    }
+    changeState = () =>{
+        this.setState({
+            // isActive:true
+            isActive:!this.state.isActive // changing the booleon value to inverse
+        })
+    }
     render(){
         // if there are multiple properties to be added we can define a simple obj and use in the style
-        let obj1 ={
+        // let obj1 ={
+        //     color:"red",
+        //     backgroundColor:"yellow" //Note Property name must be camelCased (eg:backgroundColor for background-color)
+        // };
+
+        // let obj2 ={
+        //    fontSize:"50px",
+        //    fontFamily:"cooper"
+        //  };
+        let obj ={
             color:"red",
-            backgroundColor:"yellow" //Note Property name must be camelCased (eg:backgroundColor for background-color)
-        };
-         let obj2 ={
+            backgroundColor:"yellow",            
             fontSize:"50px",
-            fontFamily:"cooper"
-          };
+        };
+        if(this.state.isActive) obj.backgroundColor = "blue"; 
         return(
             <div>
                 {/* first set of curly brace is for JS expression and secand one is for JS object  */}
                 {/* <h1 style={{color:"red", backgroundColor:"yellow"}}>Hello EveryOne</h1> */}
                 {/* <h1 style={obj}>Hello EveryOne</h1> */}
-                <h1 style={{...obj1,...obj2 }}>Hello EveryOne</h1>  {/* if we have multiple set of oject we can us spread opeartor */}
+                {/* <h1 style={{...obj1,...obj2 }}>Hello EveryOne</h1>  if we have multiple set of oject we can us spread opeartor */}
+                <button style={obj} onClick={this.changeState}>Click</button>
             </div>
         );
     }
