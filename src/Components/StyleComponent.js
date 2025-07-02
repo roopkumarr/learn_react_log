3055557377b87bc68c4711aca98ddcc86f29fde8
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-03 01:31:27 +0530

    using spread opeartor to merge multiple js object

--- src/Components/StyleComponent.js
+++ src/Components/StyleComponent.js
@@ -3,15 +3,20 @@ import React,{Component} from "react";
 class StyleComponent extends Component {
     render(){
         // if there are multiple properties to be added we can define a simple obj and use in the style
-        let obj ={
+        let obj1 ={
             color:"red",
-            backgroundColor:"yellow"
+            backgroundColor:"yellow" //Note Property name must be camelCased (eg:backgroundColor for background-color)
         };
+         let obj2 ={
+            fontSize:"50px",
+            fontFamily:"cooper"
+          };
         return(
             <div>
                 {/* first set of curly brace is for JS expression and secand one is for JS object  */}
                 {/* <h1 style={{color:"red", backgroundColor:"yellow"}}>Hello EveryOne</h1> */}
-                <h1 style={obj}>Hello EveryOne</h1>
+                {/* <h1 style={obj}>Hello EveryOne</h1> */}
+                <h1 style={{...obj1,...obj2 }}>Hello EveryOne</h1>  {/* if we have multiple set of oject we can us spread opeartor */}
             </div>
         );
     }
