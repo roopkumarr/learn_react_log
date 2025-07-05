a62834e5d22ef11463b84f5b857bd1c73317cbff
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-06 00:14:11 +0530

    added a comment

--- src/Components/Stylesheet1.js
+++ src/Components/Stylesheet1.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import Style from './Stylesheet1.module.css'
+// Note: when implementing CSS as module the .css extention should be prefixed with .module as shown above.
+// the bellow import method doesn't work
+// import Style from './Stylesheet2.css'
+// when we have multiple css files with simlar class name it doesn't work
+// to overcome this we use css module method
+
 class Stylesheet1 extends Component{
     render(){
         return(
