e4ed42913283d6accea8173f82ad0d15d3865de3
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-11 00:30:41 +0530

    calling a function to update the state

--- src/Components/UScomponent.js
+++ src/Components/UScomponent.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 
 function UScomponent(){
     const myArray =  useState("World");
+    let changeState =()=>{
+        myArray[1]("Globe")
+    }
     return(
         <div>
             <h1>
                 {myArray[0]}
             </h1>
-            <button onClick={()=> myArray[1]("Globe")}>Update</button>
+            {/* <button onClick={()=> myArray[1]("Globe")}>Update</button> */}
+            <button onClick={changeState}>Update</button>
         </div>
     )
 }
