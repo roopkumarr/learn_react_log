8370bc467497cb200254e8298b732b2e8c513a91
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-11 00:34:42 +0530

    made simple useState hook by destructring

--- src/Components/UScomponent.js
+++ src/Components/UScomponent.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 function UScomponent(){
-    const myArray =  useState("World");
+    // const myArray =  useState("World");
+    const [name, setName] =  useState("World"); //Destructuring useState hook
+
     let changeState =()=>{
-        myArray[1]("Globe")
+        // myArray[1]("Globe")
+        setName("Globe")
     }
     return(
         <div>
             <h1>
-                {myArray[0]}
+                {/* {myArray[0]} */}
+                {name}
             </h1>
             {/* <button onClick={()=> myArray[1]("Globe")}>Update</button> */}
             <button onClick={changeState}>Update</button>
