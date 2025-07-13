f46d9bf42d0b0ebb1e7efe430ae2a85e5b9a420e
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-14 00:07:50 +0530

    updation of the Person obj onclick event

--- src/Components/USobject.js
+++ src/Components/USobject.js
@@ -8,12 +8,21 @@ function USobject(){
         isYoung:true
     }
     const [person, setPerson] = useState(obj);
+    let changeState = ()=>{
+        setPerson({
+            name:"Kumar",
+            age:30,
+            height:5.3,
+            isYoung:false
+        })
+    }
     return(
         <div>
             <h1>{person.name}</h1>
             <h1>{person.age}</h1>
             <h1>{person.height}</h1>
-            <h1>{person.isYoung.toString()}</h1>
+            <h1>{person.isYoung.toString()}</h1>  {/* to make the booleon value visible we are converting it string */}
+            <button onClick={changeState}>Update</button>
         </div>
     )
 }
