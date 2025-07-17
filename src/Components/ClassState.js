c40d87a4545b38731fd9e1a9471f101cd2b93983
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-17 23:41:48 +0530

    made some simple functions to set the state variable in class component

--- src/Components/ClassState.js
+++ src/Components/ClassState.js
@@ -7,10 +7,22 @@ class ClassState extends Component{
             counter:0
         }
     }
+    Increase=()=>{
+        this.setState({
+            counter:this.state.counter+1
+        })
+    }
+    Decrease=()=>{
+        this.setState({
+            counter:this.state.counter-1
+        })
+    }
     render(){
         return(
             <div>
                 <h1>{this.state.counter}</h1>
+                <button onClick={this.Increase}>Increment</button>
+                <button onClick={this.Decrease}>Decrement</button>
             </div>
         )
     }
