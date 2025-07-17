3d097ca394b970f2335ac4fba3b5dd422b66d615
rroopkumar <rpkmrroopkumar8@gmail.com>
2025-07-17 23:52:32 +0530

    made some simple functions to set the state variable in functional component

--- src/Components/FunctionState.js
+++ src/Components/FunctionState.js
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 
 function FunctionState(){
     let [counter,setCounter] = useState(0);
+    let Increase =()=>{
+        setCounter(counter+1);
+    }
+    let Decrease =()=>{
+        setCounter(counter-1);
+    }
     return(
         <div>
         <h1>{counter}</h1>
+        <button onClick={Increase}>Increment</button>
+        <button onClick={Decrease}>Decrement</button>
         </div>
     )
 }
